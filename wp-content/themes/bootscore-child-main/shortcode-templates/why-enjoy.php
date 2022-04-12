5669b806b88342954db1cdc02585c25d7bf98627
Matias <ifran2855@gma.com>
2022-04-11 21:19:05 -0300

    tocando mas css

--- wp-content/themes/bootscore-child-main/shortcode-templates/why-enjoy.php
+++ wp-content/themes/bootscore-child-main/shortcode-templates/why-enjoy.php
@@ -1,14 +1,14 @@
-<div class="container h-100">
-    <div class="row h-100">
-
-        <div class="col-md-5 d-flex align-items-center">
-            <h2 class='title_block_two'><?php echo get_field('title_block_2')?></h2>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 d-flex align-items-center">
+            <h2 class='title_block_two w-75'><?php echo get_field('title_block_2')?></h2>
         </div>
-        <div class="col-md-7 d-flex align-self-center flex-column">
+        <div class="col-md-6">
             <h4 class='text_one_block_2 mb-4'><?php echo get_field('text_one_block_2')?></h4>
-            <h4 class='text_two_block_2 mb-4 p-4'><?php echo get_field('text_two_block_2')?></h4>
+            <div class="block_purple">
+                <h4 class='text_two_block_2 mb-4 p-4'><?php echo get_field('text_two_block_2')?></h4>
+            </div>
             <h4 class='text_three_block_2'><?php echo get_field('text_three_block_2')?></h4>
         </div>
-
     </div>
 </div>
