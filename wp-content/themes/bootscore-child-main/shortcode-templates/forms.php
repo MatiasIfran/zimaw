84b74f3dd757691e40903194d9c40d020a0026b6
Matias <ifran2855@gma.com>
2022-04-12 22:12:44 -0300

    forms

--- wp-content/themes/bootscore-child-main/shortcode-templates/forms.php
+++ wp-content/themes/bootscore-child-main/shortcode-templates/forms.php
@@ -3,6 +3,6 @@
         <div class="col-sm-6 d-flex align-items-center">
             <h2 class='title_block_seven w-75'><?php echo get_field('title_block_7')?></h2>
         </div>
-        <div class='col-sm-6 contact_form'><?php echo do_shortcode('[wpforms id="305"]')?></div>
+        <div class='col-sm-6 contact_form'><?php echo do_shortcode( get_field('shortcode_forms'))?></div>
     </div>
 </div>
