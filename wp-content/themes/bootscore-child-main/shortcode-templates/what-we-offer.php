28d031f02ad99b1c429ed07951c7dca6ddaeb560
Matias <ifran2855@gma.com>
2022-04-12 21:36:33 -0300

    header + plugin de header

--- wp-content/themes/bootscore-child-main/shortcode-templates/what-we-offer.php
+++ wp-content/themes/bootscore-child-main/shortcode-templates/what-we-offer.php
@@ -3,8 +3,8 @@
     <h2 class='title_block_three my-5'><?php echo get_field('title_block_3')?></h2>
 
     <div class="row">
-        <div class="col-md-6 col-xl-4 d-md-flex">
-            <div class="card shadow p-2 mb-5  rounded">
+        <div class="col-lg-4 col-md-6 my-3 d-flex">
+            <div class="card shadow w-100 rounded">
                 <div class="card-body d-flex align-items-center flex-row">
                     <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px"
                         viewBox="0 0 24 24" width="24px" fill="#000000">
@@ -18,8 +18,8 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-6 col-xl-4 d-md-flex">
-            <div class="card shadow p-2 mb-5  rounded">
+        <div class="col-lg-4 col-md-6 my-3 d-flex">
+            <div class="card shadow w-100 rounded">
                 <div class="card-body d-flex align-items-center flex-row">
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"
                         fill="#000000">
@@ -31,8 +31,8 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-6 col-xl-4 d-md-flex">
-            <div class="card shadow p-2 mb-5  rounded">
+        <div class="col-lg-4 col-md-6 my-3 d-flex">
+            <div class="card shadow w-100 rounded">
                 <div class="card-body d-flex align-items-center flex-row">
                     <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" height="24px" width="24px"
                         viewBox="0 0 512 512">
@@ -52,8 +52,8 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-6 col-xl-4 d-md-flex">
-            <div class="card shadow p-2 mb-5  rounded">
+        <div class="col-lg-4 col-md-6 my-3 d-flex">
+            <div class="card shadow w-100 rounded">
                 <div class="card-body d-flex align-items-center flex-row">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-headset"
                         viewBox="0 0 16 16">
@@ -64,8 +64,8 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-6 col-xl-4 d-md-flex">
-            <div class="card shadow p-2 mb-5 rounded">
+        <div class="col-lg-4 col-md-6 my-3 d-flex">
+            <div class="card shadow w-100 rounded">
                 <div class="card-body d-flex align-items-center flex-row">
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"
                         fill="#000000">
@@ -77,8 +77,8 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-6 col-xl-4 d-md-flex">
-            <div class="card shadow p-2 mb-5  rounded">
+        <div class="col-lg-4 col-md-6 my-3 d-flex">
+            <div class="card shadow w-100 rounded">
                 <div class="card-body d-flex align-items-center flex-row">
                     <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512">
                         <path
