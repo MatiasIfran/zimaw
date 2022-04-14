de8f69dd6dba8f987a0ce054717ded6a002bab12
Matias <ifran2855@gma.com>
2022-04-13 21:36:06 -0300

    plugin forms + css

--- wp-content/themes/bootscore-child-main/shortcode-templates/forms.php
+++ wp-content/themes/bootscore-child-main/shortcode-templates/forms.php
@@ -1,8 +1,12 @@
 <div class="container h-100 py-5">
     <div class="row">
-        <div class="col-sm-6 d-flex align-items-center">
-            <h2 class='title_block_seven w-75'><?php echo get_field('title_block_7')?></h2>
+        <div class="col-md-6 d-flex align-items-center">
+            <h2 class='title_block_seven w-md-75'><?php echo get_field('title_block_7')?></h2>
         </div>
-        <div class='col-sm-6 contact_form'><?php echo do_shortcode( get_field('shortcode_forms'))?></div>
+        <div class='col-md-6 background-form d-flex justify-content-center'>
+            <div class="contact_form">
+                <?php echo do_shortcode( get_field('shortcode_forms'))?>
+            </div>
+        </div>
     </div>
 </div>
