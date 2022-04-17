783744c5de33974a449e5482ab1384ea37266c16
Matias <ifran2855@gma.com>
2022-04-17 19:37:32 -0300

    agregando svg

--- wp-content/themes/bootscore-child-main/shortcode-templates/we_work.php
+++ wp-content/themes/bootscore-child-main/shortcode-templates/we_work.php
@@ -7,36 +7,36 @@
 
         <div class="col-md-7 d-flex align-self-center flex-column">
             <div class="card mb-3 d-flex">
-                <div class="row" style="margin-left: -6px;">
+                <div class="row" style="margin-left: 0px;">
                     <div class="col-2 bookmark_svg"> 
                         <p class="bookmark_text"><span>1</span></p>
                     </div>  
-                        <p class="col-10 text-left p-3 m-0"><?php echo get_field('text_one_block_6')?></p>
+                        <p class="col-10 text-left p-4 m-0"><?php echo get_field('text_one_block_6')?></p>
                 </div>
             </div>
             <div class="card mb-3 d-flex">
-                <div class="row" style="margin-left: -6px;">
+                <div class="row" style="margin-left: 0px;">
                     <div class="col-2 bookmark_svg"> 
                         <p class="bookmark_text"><span>2</span></p>
                     </div>  
-                        <p class="col-10 text-left p-3 m-0"><?php echo get_field('text_two_block_6')?></p>
+                        <p class="col-10 text-left p-4 m-0"><?php echo get_field('text_two_block_6')?></p>
                 </div>
             </div>
             <div class="card mb-3 d-flex">
-                <div class="row" style="margin-left: -6px;">
+                <div class="row" style="margin-left: 0px;">
                     <div class="col-2 bookmark_svg"> 
                         <p class="bookmark_text"><span>3</span></p>
                     </div>  
-                        <p class="col-10 text-left p-3 m-0"><?php echo get_field('text_three_block_6')?></p>
+                        <p class="col-10 text-left p-4 m-0"><?php echo get_field('text_three_block_6')?></p>
                 </div>
             </div>
             <?php if(get_field('text_four_block_6')): ?>
                 <div class="card mb-3 d-flex">
-                <div class="row" style="margin-left: -6px;">
+                <div class="row" style="margin-left: 0px;">
                     <div class="col-2 bookmark_svg"> 
                         <p class="bookmark_text"><span>4</span></p>
                     </div>  
-                        <p class="col-10 text-left p-3 m-0"><?php echo get_field('text_four_block_6')?></p>
+                        <p class="col-10 text-left p-4 m-0"><?php echo get_field('text_four_block_6')?></p>
                 </div>
             </div>
             <?php endif;?>
