c73f798d447709143d13c111cca16d3b738dc606
Matias <ifran2855@gma.com>
2022-04-17 20:54:44 -0300

    cambiando algo de css

--- wp-content/themes/bootscore-child-main/shortcode-templates/why-enjoy.php
+++ wp-content/themes/bootscore-child-main/shortcode-templates/why-enjoy.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-6 d-flex align-items-center">
-            <h2 class='title_block_two w-75'><?php echo get_field('title_block_2')?></h2>
+            <h2 class='title_block_two'><?php echo get_field('title_block_2')?></h2>
         </div>
         <div class="col-md-6">
             <h4 class='text_one_block_2 my-4'><?php echo get_field('text_one_block_2')?></h4>
