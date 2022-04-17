6d55e84f0b4a3d96ff1d0b404ccb6ea229510a1e
Matias <ifran2855@gma.com>
2022-04-17 19:47:26 -0300

    tocando responsive

--- wp-content/themes/bootscore-child-main/shortcode-templates/why-enjoy.php
+++ wp-content/themes/bootscore-child-main/shortcode-templates/why-enjoy.php
@@ -4,11 +4,11 @@
             <h2 class='title_block_two w-75'><?php echo get_field('title_block_2')?></h2>
         </div>
         <div class="col-md-6">
-            <h4 class='text_one_block_2 mb-4'><?php echo get_field('text_one_block_2')?></h4>
+            <h4 class='text_one_block_2 my-4'><?php echo get_field('text_one_block_2')?></h4>
             <div class="block_purple">
-                <h4 class='text_two_block_2 mb-4 p-4'><?php echo get_field('text_two_block_2')?></h4>
+                <h4 class='text_two_block_2 my-4 p-4'><?php echo get_field('text_two_block_2')?></h4>
             </div>
-            <h4 class='text_three_block_2'><?php echo get_field('text_three_block_2')?></h4>
+            <h4 class='text_three_block_2 my-4'><?php echo get_field('text_three_block_2')?></h4>
         </div>
     </div>
 </div>
