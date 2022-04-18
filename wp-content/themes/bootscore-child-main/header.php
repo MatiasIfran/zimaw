d589931ceee1beddde49cb7e3c35c3e8a7862c58
Matias <ifran2855@gma.com>
2022-04-18 20:27:20 -0300

    arreglando video

--- wp-content/themes/bootscore-child-main/header.php
+++ wp-content/themes/bootscore-child-main/header.php
@@ -51,7 +51,6 @@
             <!-- Offcanvas Navbar -->
             <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvas-navbar">
               <div class="offcanvas-header ">
-                <span class="h5 mb-0"><?php esc_html_e('Menu', 'bootscore'); ?></span>
                 <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
               </div>
               <div class="offcanvas-body d-lg-flex justify-content-md-end">
@@ -97,7 +96,7 @@
               <button class="navbar_toggler btn d-lg-none ms-1 ms-md-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas-navbar" aria-controls="offcanvas-navbar">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
                   <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-                </svg><span class="visually-hidden-focusable">Menu</span>
+                </svg>
               </button>
 
             </div><!-- .header-actions -->
