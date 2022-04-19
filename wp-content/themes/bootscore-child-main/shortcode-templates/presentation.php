556bc52f4f4a8a5bdeb35a5fef8f677f739bba9a
Matias <ifran2855@gma.com>
2022-04-19 09:21:08 -0300

    mas css v2

--- wp-content/themes/bootscore-child-main/shortcode-templates/presentation.php
+++ wp-content/themes/bootscore-child-main/shortcode-templates/presentation.php
@@ -1,4 +1,4 @@
-<div class="container w-75 text-center">
+<div class="container text-center">
     <h2 class='title_block_one'><?php echo get_field('title_block_1')?></h2>
     <h4 class='sub_title_block_one'><?php echo get_field('sub_title_block_1')?></h4>
 </div>
